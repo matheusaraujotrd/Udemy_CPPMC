06a51cd0dc8a5f52b4e17d183a5d1fa9e5d173a7
Matheus Araujo <matheusaraujotradutor@gmail.com>
2022-07-10 16:39:01 -0300

    Remove an unnecessary line break. Not much to do, simple github testing.

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -6,7 +6,6 @@ int main() {
 std::string country;
 std::cout << "Where do you live?" << std::endl;
 std::cin >> country;
-std::cout << std::endl;
 std::cout << "I've heard great things about " << country << ". I'd like to go sometime.";
 return 0;
 }
